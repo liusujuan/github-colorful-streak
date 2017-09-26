58a78ad4c056740843d84be918d33e6de27d4c0b
mingfei <mingfei.net@gmail.com>
2017-09-27 00:50:33 +0800

    Update

--- options.js
+++ options.js
@@ -1,29 +1,36 @@
 // Saves options to localStorage.
-function save_options() {
-    var select = document.getElementById("color");
-    var index = select.children[select.selectedIndex].value;
-    chrome.storage.sync.set({'index': index});
-    var status = document.getElementById("status");
-    status.innerHTML = 'Options Saved.';
-    setTimeout(function () {
-        status.innerHTML = '';
-    }, 750);
+function saveOptions(id, color) {
+    console.log('saveOptions: ' + id + ", " + color);
+    // var select = document.getElementById("color");
+    // var index = select.children[select.selectedIndex].value;
+    // chrome.storage.sync.set({'index': index});
+    // var status = document.getElementById("status");
+    // status.innerHTML = 'Options Saved.';
+    // setTimeout(function () {
+    //     status.innerHTML = '';
+    // }, 750);
 }
 
 // Restores select box state to saved value from localStorage.
-function restore_options() {
-    chrome.storage.sync.get(['index'], function (items) {
-        var index = items['index'];
-        var select = document.getElementById('color');
-        for (var i = 0; i < select.children.length; i++) {
-            var child = select.children[i];
-            if (child.value === index) {
-                child.selected = 'true';
-                break;
-            }
-        }
-    });
+function restoreOptions() {
+    console.log('load...');
+    // chrome.storage.sync.get(['index'], function (items) {
+    //     var index = items['index'];
+    //     var select = document.getElementById('color');
+    //     for (var i = 0; i < select.children.length; i++) {
+    //         var child = select.children[i];
+    //         if (child.value === index) {
+    //             child.selected = 'true';
+    //             break;
+    //         }
+    //     }
+    // });
 }
 
-window.addEventListener('load', restore_options);
-document.getElementById('button').addEventListener('click', save_options);
+window.addEventListener('load', restoreOptions);
+for (var i = 0; i < document.querySelectorAll('div[title]').length; i++) {
+    var div = document.querySelectorAll('div[title]')[i];
+    div.addEventListener('click', function (e) {
+        saveOptions(e.target.parentElement.id, e.target.parentElement.title);
+    });
+}
